38cfcc14a5acfe561f902d6f0b790e6b5d4e6699
Rachel <rachel.rolon@gmail.com>
2020-11-12 21:35:09 -0600

    trying to get validation to work

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        //Get: "/user/index"
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.Username = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords do not match. Try again.";
+                ViewBag.Username = newUser.Username;
+                ViewBag.Email = newUser.Email;
+
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get;  }
+        public int ID { get; }
+        private static int nextID = 0;
+
+        public User()
+        {
+            this.ID = nextID++;
+        }
+
+        public User(string username, string email, string pasword) : this()
+        {
+            Username = username;
+            Email = email;
+            Password = pasword;
+        }
+
+
+    }
+}
